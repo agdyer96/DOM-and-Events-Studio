944c1b616129c5a06fd9b42c1e36aa151bbea897
Adam Dyer <agdyer96@gmail.com>
2023-02-23 19:52:14 -0600

    added html

--- scripts.js
+++ scripts.js
@@ -1,2 +1,32 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+document.addEventListener("DOMContentLoaded", function() {
+    const takeoffButton = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackround = document.getElementById("shuttleBackround");
+    const landButton = document.getElementById("landing");
+    const abortButton = document.getElementById("missionAbort");
+    takeoffButton.addEventListener("click", function() {
+        let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if(response) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            flightStatus.style.color = 'red';
+            shuttleBackround.style.backgroundColor = 'blue';
+            
+        }
+    });
+
+    landButton.addEventListener("click", function() {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackround.style.backgroundColor = 'green';
+    });
+
+    abortButton.addEventListener("click", function() {
+        let response = window.confirm("Confirm that you want to abort the mission.");
+        if (response) {
+            flightStatus.innerHTML = "Mission aborted.";
+            shuttleBackround.style.backgroundColor = 'red';
+        }
+    });
+
+
+});
